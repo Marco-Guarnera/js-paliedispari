e726a0b5ee1292c78e1af21ea612c8d53b00eef0
Marco-Guarnera <Darkaquon93@gmail.com>
2024-08-22 15:59:08 +0200

    first commit

---
+++ js/exercise01.js
@@ -0,0 +1,12 @@
+// Esercizio 01 - Palindromo
+
+/*
+1. Domandare all'utente una parola
+2. Creare una funzione per capire se la parola inserita è un palindromo
+*/
+
+// Fase di preparazione
+// Fase di gestione eventi
+// Fase di raccolta dati
+// Fase di elaborazione
+// Fase di produzione
---
+++ js/exercise02.js
@@ -0,0 +1,15 @@
+// Esercizio 02 - Pari e Dispari
+
+/*
+1. Domandare all'utente di scegliere tra pari e dispari e un numero da 1 a 5
+2. Creare una funzione per generare un numero random da 1 a 5 per il computer
+3. Sommare i due numeri
+4. Creare una funzione per capire se la somma dei due numeri è pari o dispari
+5. Stampare il vincitore
+*/
+
+// Fase di preparazione
+// Fase di gestione eventi
+// Fase di raccolta dati
+// Fase di elaborazione
+// Fase di produzione
